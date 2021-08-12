7f7a17477ea4b555ad6d489f006cb873e4dfdc43
Ricardo <ricardosaca@gmail.com>
2021-08-12 17:16:10 -0400

    Add route

--- app.py
+++ app.py
@@ -15,12 +15,21 @@ db = SQLAlchemy(app)
 admin = Admin(app)
 
 class Book(db.Model):
+    __tablename__ = 'books'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     author = db.Column(db.String(30))
     date_finished = db.Column(db.DateTime)
 
+db.init_app()
+
 admin.add_view(ModelView(Book, db.session))
+
+@app.route('/')
+def index():
+    return "Hello"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
