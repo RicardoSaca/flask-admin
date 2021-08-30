3615c23a1ea82a63ce2a869a913193c0559e1e49
Ricardo <ricardosaca@gmail.com>
2021-08-30 13:50:26 -0400

    added tablename to users

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,6 +4,7 @@ from portfolio import login
 from portfolio import db
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
